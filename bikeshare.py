16d2cf1528fbf214debde1ec4583652dcbb6cee9
eriharizi <130974885+eriharizi@users.noreply.github.com>
2026-05-18 00:09:58 +0200

    refactor: add module description comment

--- bikeshare.py
+++ bikeshare.py
@@ -2,6 +2,8 @@ import time
 import pandas as pd
 import numpy as np
 
+# This program analyzes bikeshare data for Chicago, New York City, and Washington
+
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
